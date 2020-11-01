3730e8038d741defb80ac7d2e1438e6759b7b7ff
Juan <64536309+juanjies@users.noreply.github.com>
2020-11-01 15:23:07 -0500

    Add files via upload

--- proj3.cpp
+++ proj3.cpp
@@ -6,23 +6,47 @@ using namespace std;
 // Purpose: EECS402 Project 3 - Use dynamic allocation to implement image
 //          editing with a UNIX Makefile to link multiple .h and .cpp files. 
 
+const int igoredCharLen = 300;
+
 int main ()  {
   string fileName;
   ifstream inFile;
   string magicNum; // temp
   int imageWid = 0, imageLen = 0; // temp
+  bool isValidInput = false;
 
   cout << "Enter string for PPM image file name to load: " << endl;
   cin >> fileName;
   
   inFile.open("fileName.c_str()"); 
   
-  // inFile >> magicNum;
-  // cout << "Magic Number is: " << magicNum << endl;
-  inFile >> imageWid;
-  inFile >> imageLen;
-  cout << "Image Width = " << imageWid << endl;
-  cout << "Image Length = " << imageLen << endl;
+  if (inFile.fail())  {
+		cout << "Unable to open input file!" << endl;
+    exit;
+	}
+	
+  while (!isValidInput)  {
+        
+    // inFile >> magicNum;
+    // cout << "Magic Number is: " << magicNum << endl;
+    inFile >> imageWid;
+    inFile >> imageLen;
+
+    if (inFile.eof())  {
+			cout << "EOF before reading the image" << endl;
+      exit(2);
+		}
+		else if (inFile.fail())  {
+			inFile.clear();
+      inFile.ignore(igoredCharLen, '\n');
+		}
+    else  {
+      isValidInput = true;
+    }
+		
+    cout << "Image Width = " << imageWid << endl;
+    cout << "Image Length = " << imageLen << endl;
+	}
 
   inFile.close();
   
