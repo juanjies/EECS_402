f6342e648a36c04d7d6f96de65bb71e73791fa60
Juan <64536309+juanjies@users.noreply.github.com>
2020-11-10 09:04:29 -0500

    Add files via upload

--- showMenu.cpp
+++ showMenu.cpp
@@ -11,22 +11,23 @@ int showMenu()  {
   int topMenuOpt = 0;  
   bool isValidInput = false;
 
+  cout << "1. Annotate image with rectangle" << '\n'
+         << "2. Annotate image with pattern from file" << '\n'
+         << "3. Insert another image" << '\n'
+         << "4. Write out current image" << '\n'
+         << "5. Exit the program" << '\n'
+         << "Enter int for main menu choice: " << endl;
+
   while (!isValidInput)  {
-    cout << "1. Annotate image with rectangle" << '\n'
-       << "2. Annotate image with pattern from file" << '\n'
-       << "3. Insert another image" << '\n'
-       << "4. Write out current image" << '\n'
-       << "5. Exit the program" << '\n'
-       << "Enter int for main menu choice: " << endl;
     
     cin  >> topMenuOpt;
     // error cheching for cin
     if (cin.fail())  {
       cin.clear();
-      cin.ignore(IGNORED_CHAR_LEN);
+      cin.ignore(IGNORED_CHAR_LEN, '\n');
       cout << "Error found when reading the input" 
-           << " - expected a integer between 1 and 5"
-           << " but found " << topMenuOpt << endl;
+           << " - expected a integer between 1 and 5" << '\n'
+           << "try again" << endl;
     }
     else  {
       isValidInput = true;
